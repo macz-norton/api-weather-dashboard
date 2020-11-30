a298481dd4297f4d1772ad2dded920f4c719e97c
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-29 18:30:51 -0800

    Set up local storage for user city search

--- assets/script.js
+++ assets/script.js
@@ -12,17 +12,11 @@
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
 
-// form
-// pull user input
-
-// This function handles events where one button is clicked
-$("#city-button").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
+// When user clicks `citySearch` button
+$("#citySearch").on("click", function(event) {
     event.preventDefault();
 
-    // This line will grab the text from the input box
-    var userCity = $("#user-input").val().trim();
+    var userCity = $(".userInput").val().trim();
     console.log(userCity);
 
     JSON.stringify(userCity);
@@ -30,87 +24,78 @@ $("#city-button").on("click", function(event) {
 
     var userCitySaved = localStorage.getItem("city");
     console.log(userCitySaved);
-    // $(".list-group-item").text(userCitySaved);
 
-    // for (var i = 0; i < userCitySaved.length; i++) {
-
-    // Then dynamicaly generating buttons for each movie in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-    var button = $("<button>");
-    // Adding a class
-    button.addClass("city-list");
-    // Adding a data-attribute with a value of the movie at index i
-    button.attr("data-city", userCitySaved);
-    // Providing the button's text with a value of the movie at index i
-    button.text(userCitySaved);
-    // Adding the button to the HTML
-    $("#search-list").append(button);
-    // }
-
+    var cityButton = $("<button>");
+    cityButton.attr("type", "button");
+    cityButton.addClass("btn btn-light btn-lg btn-block");
+    cityButton.attr("data-city", userCitySaved);
+    cityButton.text(userCitySaved);
+    $(".searchList").prepend(cityButton);
+    
 });
 
-function getWeather(){
+// function getWeather(){
 
-    var city = "";
-    var APIkey = "";
-    var queryCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+//     var city = "";
+//     var APIkey = "";
+//     var queryCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
     
-    $.ajax({
-        url: queryCurrent,
-        method: "GET"
-    }).then(function(response) {
+//     $.ajax({
+//         url: queryCurrent,
+//         method: "GET"
+//     }).then(function(response) {
     
-        var cityNameEl = $("<h2>").text(response.name);
-        var weatherIconEl = $("<i>").icon(response.weather.icon);
-        var temperatureEl = $("<p>").text(response.list.main.temp.metric);
-        var humidityEl = $("<p>").text(response.list.main.humidity);
-        var windSpeedEl = $("<p>").text(response.list.wind.speed);
-        // var uvIndexEl = $("<p>").text(
+//         var cityNameEl = $("<h2>").text(response.name);
+//         var weatherIconEl = $("<i>").icon(response.weather.icon);
+//         var temperatureEl = $("<p>").text(response.list.main.temp.metric);
+//         var humidityEl = $("<p>").text(response.list.main.humidity);
+//         var windSpeedEl = $("<p>").text(response.list.wind.speed);
+//         // var uvIndexEl = $("<p>").text(
     
-    })
+//     })
 
-}
+// }
 
 
 
-var city = "";
-var APIkey = "";
-var query5day = "api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=5&appid=" + APIkey;
+// var city = "";
+// var APIkey = "";
+// var query5day = "api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=5&appid=" + APIkey;
 
-$.ajax({
-    url: query5day,
-    method: "GET"
-}).then(function(response) {
+// $.ajax({
+//     url: query5day,
+//     method: "GET"
+// }).then(function(response) {
 
-    var dateEl = $("<h3>").text(response.list.dt);
-    var weatherEl = $("<i>").i(response.list.weather.icon);
-    var temperatureEl = $("<p>").text(response.list.main.temp.metric);
-    var humidityEl = $("<p>").text(response.list.main.humidity);
-});
+//     var dateEl = $("<h3>").text(response.list.dt);
+//     var weatherEl = $("<i>").i(response.list.weather.icon);
+//     var temperatureEl = $("<p>").text(response.list.main.temp.metric);
+//     var humidityEl = $("<p>").text(response.list.main.humidity);
+// });
 
-var latitude = "";
-var longitude = "";
-var APIkey = "";
-var query5day = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
+// var latitude = "";
+// var longitude = "";
+// var APIkey = "";
+// var query5day = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
 
-$.ajax({
-    url: query5day,
-    method: "GET"
-}).then(function(response) {
+// $.ajax({
+//     url: query5day,
+//     method: "GET"
+// }).then(function(response) {
 
-    var dateEl = $("<h3>").text(response.list.dt);
+//     var dateEl = $("<h3>").text(response.list.dt);
 
-three variables that will get the data from each of the three APIs. 
-var people, places, reports 
-wait for all queries to be done before doing something with the data.
-$.when( 
-    $.get(url_1, data => people = data),
-    $.get(url_2, data => places = data),
-    $.get(url_3, data => reports = data)
-).then(function() {
-    ...
+// three variables that will get the data from each of the three APIs. 
+// var people, places, reports 
+// wait for all queries to be done before doing something with the data.
+// $.when( 
+//     $.get(url_1, data => people = data),
+//     $.get(url_2, data => places = data),
+//     $.get(url_3, data => reports = data)
+// ).then(function() {
+//     ...
 
-// Use bootstrap classes in JavaScript 
+// // Use bootstrap classes in JavaScript 
 
-var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
-var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+// var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+// var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");

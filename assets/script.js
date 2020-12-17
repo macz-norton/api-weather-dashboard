b0b7be52a231ce65f1a3c89279e4449acc94e809
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-12-17 13:54:42 -0800

    Updated placement of current day icon

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,9 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
-
+// Define global variables
 var lat;
 var lon;
 var citySearched;
 
 // When user clicks `citySearch` button
-
 $("#citySearch").click(function(event) {
     event.preventDefault();
 
@@ -28,11 +14,12 @@ $("#citySearch").click(function(event) {
     JSON.stringify(userCity);
     localStorage.setItem("city", userCity);
 
+    // Run `getCurrentWeather` to query API
     getCurrentWeather(citySearched);
 
 });
 
-
+// Function to query Current Weather API
 function getCurrentWeather(citySearched) {
 
     // Get `userCitySaved` out of local storage
@@ -45,10 +32,9 @@ function getCurrentWeather(citySearched) {
     cityListItem.text(citySearched);
     $(".searchList").prepend(cityListItem);
 
+    // Variables for query URL
     var APIkey = "d5fdfbd079865261527ef46dccc3c543";
     var queryCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&appid=" + APIkey;
-
-    console.log(queryCurrentWeather);
 
     $.ajax({
         url: queryCurrentWeather,
@@ -61,7 +47,8 @@ function getCurrentWeather(citySearched) {
         var currentHumidity = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + "%");
         var currentWindSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + response.wind.speed + " MPH");
        
-        $(".currentWeather").append(currentSearch, currentIcon, currentTemp, currentHumidity, currentWindSpeed);
+        $(".currentWeather").append(currentSearch, currentTemp, currentHumidity, currentWindSpeed);
+        $(currentSearch).append(currentIcon);
        
         lon = response.coord.lon;
         lat = response.coord.lat;
